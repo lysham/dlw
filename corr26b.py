2f172fcda24d543fd6ee4681657b0b7d236d8f11
Lysha Matsunobu <lmatsuno@eng.ucsd.edu>
2023-03-24 12:57:46 -0700

    Add Shakespeare method to find optical depth
    Add clear sky selection, implement calculation of tau, compare

--- corr26b.py
+++ corr26b.py
@@ -11,10 +11,11 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
-from scipy import integrate
+from scipy import integrate, interpolate
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from copy import deepcopy
+from scipy.io import loadmat
 
 from main import SIGMA, get_pw, get_esky_c, li_lw, CORR26A
 from pcmap_data_funcs import get_asos_stations
@@ -157,13 +158,9 @@ def process_site(site, yr="2012"):
             ['zen', 'direct_n', 'diffuse', 'dw_ir', 'dw_solar', 'temp',
              'rh', 'pressure']]  # reduce data columns
         df['t_a'] = df.temp + 273.15  # convert celsius to kelvin
-        # df = df[df.zen < 85]  # remove night values
 
         # remove negative GHI and DNI values
         df = df.loc[(df.dw_solar > 0) & (df.direct_n > 0)]
-
-        # Reduce sample here TODO remove later(?) (orig 1%)
-        df = df.sample(frac=0.05, random_state=96)
 
         # apply clear sky analysis
         location = pvlib.location.Location(lat, lon, altitude=alt)
@@ -174,8 +171,15 @@ def process_site(site, yr="2012"):
         }
         df = df.merge(cs_out, how='outer', left_index=True, right_index=True)
         df = df.rename(columns=col_rename)
-        # sdf = find_clearsky(sdf) # no clear-sky analysis for now
         print("QC and clear sky applied.", time.time() - start_time)
+
+        # Apply clear sky period filter
+        df = find_clearsky(df)
+        # need to apply clear sky filter before data is sampled
+        print("Clear sky filter applied.", time.time() - start_time)
+
+        # Reduce sample size TODO remove later(?) (orig 1%)
+        df = df.sample(frac=0.05, random_state=96)
 
         # Determine T_sky values, and correct for 3-50 micron range
         temp = df.t_a.values
@@ -206,7 +210,7 @@ def process_site(site, yr="2012"):
 
 
 def join_surfrad_asos(site="BON"):
-    # assumes SURFRAD data has already been processed
+    # assumes SURFRAD data has already been processed, assumes 2012
     usaf = SURF_ASOS[site]["usaf"]
     wban = SURF_ASOS[site]["wban"]
     # get SURFRAD data (after processing)
@@ -216,7 +220,7 @@ def join_surfrad_asos(site="BON"):
     tmp.sort_index(inplace=True)
     tmp = tmp.tz_localize("UTC")
     # get ASOS station data
-    filename = os.path.join("data", "asos_2012", f"{usaf}-{wban}-2012.csv")
+    filename = os.path.join("data", "asos_2012", f"{usaf}-{wban:05}-2012.csv")
     df = pd.read_csv(filename, skiprows=1, index_col=0, parse_dates=True)
     df = df[['CF2', 'CBH2', 'TEMP', 'VSB', 'DEWP', 'SLP', 'PERCP']]
     mask = df.index.duplicated(keep="first")
@@ -235,9 +239,9 @@ def join_surfrad_asos(site="BON"):
     df["lw_c"] = df.esky_c * SIGMA * np.power(df.t_a, 4)
 
     # drop rows with missing values in parameter columns
-    x = df.shape[0]
+    # x = df.shape[0]
     df.dropna(subset=["t_a", "rh", "cf", "lw_c", "lw_s"], inplace=True)
-    print(x, df.shape[0])
+    # print(x, df.shape[0])
     return df
 
 
@@ -293,6 +297,117 @@ def retrieve_asos_site_info(site):
     return asos_site
 
 
+def cs_metrics_1to5(obs, clr, thresholds="ghi"):
+    """
+    Supporting function to determine clear sky periods using thresholds set
+    in Section 3.2 in Li et al. 2017
+
+    Parameters
+    ----------
+    obs : series, array-like
+        Measured values in a sliding window period
+    clr : series, array-like
+        Clear ksy modeled values in a siding window period
+    thresholds : ["ghi", "dni"]
+        Indicate whether to use the GHI or DNI thresholds
+
+    Returns
+    -------
+    metrics_sum : int
+        For each condition that passes the threshold, metrics_sum is
+        incremented by one. A clear sky period needs to pass the five
+        conditions tested for in this function.
+    """
+    metrics_sum = 0
+    # threshold values from Table 3
+    if thresholds == "dni":
+        t1 = 200
+        t2 = 200
+        t3 = 100
+        t4 = 15
+        t5 = 0.015
+    else:
+        t1 = 100
+        t2 = 100
+        t3 = 50
+        t4 = 10
+        t5 = 0.01
+
+    if (np.abs(np.nanmean(obs) - np.nanmean(clr))) < t1:
+        metrics_sum += 1
+
+    if (np.abs(np.max(obs) - np.max(clr))) < t2:
+        metrics_sum += 1
+
+    tmp = (np.abs(np.abs(np.diff(obs)).sum() - np.abs(np.diff(clr)).sum()))
+    if tmp < t3:
+        metrics_sum += 1
+
+    if (np.max(np.abs(np.diff(obs) - np.diff(clr)))) < t4:
+        metrics_sum += 1
+
+    # std of slopes normalized by irradiance mean
+    t1 = np.std(np.diff(obs)) / np.mean(obs)
+    t2 = np.std(np.diff(clr)) / np.mean(clr)
+    if np.abs(t1 - t2) < t5:
+        metrics_sum += 1
+
+    return metrics_sum
+
+
+def find_clearsky(df):
+    # only applies for daytime values
+    df["day"] = df.zen < 85
+
+    starts = df.index.values
+    ends = df.index + dt.timedelta(minutes=10)  # ten-minute sliding window
+    # note that sliding window may preferentially treat shoulders of days
+    # since windows will have fewer points
+
+    cs_array = np.zeros(df.shape[0])  # start as False for clear sky
+    for i in range(0, len(starts)):
+        window_start = starts[i]
+        window_end = ends[i]
+        # select for sliding window
+        sw = df[(df.index < window_end) & (df.index >= window_start)]
+        day_check = sw.day.sum() == len(sw)  # all pts are labelled day
+        npts_check = len(sw) > 5  # at least 5min of data
+        if npts_check & day_check:
+            obs = sw.GHI_m.to_numpy()
+            clr = sw.GHI_c.to_numpy()
+            ghi_metrics_sum = cs_metrics_1to5(obs, clr, thresholds="ghi")
+
+            obs = sw.DNI_m.to_numpy()
+            clr = sw.DNI_c.to_numpy()
+            dni_metrics_sum = cs_metrics_1to5(obs, clr, thresholds="dni")
+
+            if ghi_metrics_sum + dni_metrics_sum == 10:
+                # all 10 criteria passed as true
+                for j in range(sw.shape[0]):
+                    # individually change each value in sliding window range
+                    cs_array[i+j] = 1
+    df['cs_period'] = cs_array
+    df["cs_period"] = df["cs_period"].astype('bool')
+    df = df.drop(columns=["day"])  # drop the day label column
+    return df
+
+
+def shakespeare(lat, lon):
+    filename = os.path.join("data", "shakespeare", "data.mat")
+    f = loadmat(filename)
+    # Get scale height H
+    lon_pts = f["lon"]
+    lat_pts = np.flip(f["lat"])  # must be in ascending order for interp
+    h = np.flip(f["H"], axis=1)
+    h_spline = interpolate.RectBivariateSpline(lon_pts, lat_pts, h)
+    h1 = h_spline.ev(lon, lat).item()
+    # he = (h1 / np.cos(40.3 * np.pi / 180)) * (df.p_ratio ** 1.8)
+    tau_spline = interpolate.RectBivariateSpline(
+        f["q"], f["Heff"], f["tau_eff_400"]
+    )
+    return h1, tau_spline
+
+
 if __name__ == "__main__":
     print()
 
@@ -301,17 +416,16 @@ if __name__ == "__main__":
     # import_asos_yr(yr=2012)  # import closest asos stations for a given year
     # # NOTE: specified stations may not be available for a given year
     # # find info for a specific site
-    # site = "DRA"
-
 
     # SURFRAD
-    site = "BON"
+    # sites = ["BON", "BOU", "DRA", "FPK"]
+    # for site in sites:
+    #     process_site(site=site, yr='2012')
+    # site = "BON"
     # process_site(site=site, yr='2012')
-    df = join_surfrad_asos(site)
-    model, y_true, y_pred, rmse = custom_fit(df)
+    # df = join_surfrad_asos(site)
+    # model, y_true, y_pred, rmse = custom_fit(df)
     # plot_fit(site, model.coef_, y_true, y_pred, rmse)
-
-    # TODO make a clear sky filter based on 10 tests
 
     # df = df.loc[df.zen < 85]  # remove night values
     # df["cmf"] = 1 - (df.GHI_m / df.GHI_c)
@@ -320,3 +434,93 @@ if __name__ == "__main__":
 
     # df["f"] = df.lw_s / df.dw_ir
     # df.f.hist(bins=100)
+
+    # Explore shakespeare paper approach
+    site = "FPK"
+    lat1 = SURFRAD[site]["lat"]
+    lon1 = SURFRAD[site]["lon"]
+
+    h1, spline = shakespeare(lat1, lon1)
+
+    df = join_surfrad_asos(site)
+    df = df[[
+        "rh", "pressure", "t_a", "pw", "dw_ir", "lw_s",
+        "esky_c", "lw_c", "zen", "cs_period"
+    ]]
+    df = df.rename(columns={"pressure": "pa_hpa", "pw": "pw_hpa"})
+    df["w"] = 0.62198 * df.pw_hpa / (df.pa_hpa - df.pw_hpa)
+    df["q"] = df.w / (1 + df.w)
+    p0 = 101325  # Pa
+    df["p_ratio"] = (df.pa_hpa * 100) / p0
+    df["he"] = (h1 / np.cos(40.3 * np.pi / 180)) * (df.p_ratio ** 1.8)
+    df = df.drop(columns=["p_ratio"])
+    # solve for tau at each q and he
+    tau = []
+    for q1, he1 in zip(df.q.values, df.he.values):
+        tau.append(spline.ev(q1, he1).item())
+    df["tau"] = tau
+
+    # calc emissivity
+    df["esky_t"] = 1 - np.exp(-1 * df.tau)
+    df["lw_c_t"] = df.esky_t * SIGMA * np.power(df.t_a, 4)
+
+    df["esky_day"] = 0.598 + (0.057 * np.sqrt(df.pw_hpa))
+
+    fig, ax = plt.subplots()
+    ax.grid(alpha=0.5)
+    ax.scatter(df.esky_t, df.esky_day, marker=".", alpha=0.1, label="(22a)")
+    ax.scatter(df.esky_t, df.esky_c, marker=".", alpha=0.1, label="(22c)")
+    ax.axline((0.8, 0.8), slope=1, c="0.7", ls="--")
+    ax.set_ylabel("calibrated Brunt [-]")
+    ax.set_xlabel("Shakespeare method [-]")
+    ax.set_xlim(0.5, 0.95)
+    ax.set_ylim(0.5, 0.95)
+    ax.set_axisbelow(True)
+    ax.set_title(f"{site} (npts={df.shape[0]:,})")
+    leg = ax.legend()
+    for lh in leg.legendHandles:
+        lh.set_alpha(1)
+    filename = os.path.join("figures", f"esky_clr_{site}.png")
+    fig.savefig(filename, bbox_inches="tight", dpi=300)
+    plt.close()
+    # plt.show()
+
+    pdf = df.loc[df.cs_period].copy()
+    pdf["lw_c_22a"] = pdf.esky_day * SIGMA * np.power(pdf.t_a, 4)
+
+    fig, ax = plt.subplots()
+    ax.grid(alpha=0.5)
+    ax.scatter(pdf.lw_c_t, pdf.lw_c_22a, marker=".", alpha=0.5, label="22a")
+    ax.scatter(pdf.lw_c_t, pdf.lw_c, marker=".", alpha=0.5, label="22c")
+    ax.axline((300, 300), slope=1, c="0.7", ls="--")
+    ax.set_ylabel("calibrated Brunt [W/m$^2$]")
+    ax.set_xlabel("Shakespeare method [W/m$^2$]")
+    ymin, ymax = ax.get_ylim()
+    ax.set_xlim(ymin, ymax)
+    ax.set_axisbelow(True)
+    leg = ax.legend()
+    for lh in leg.legendHandles:
+        lh.set_alpha(1)
+    ax.set_title(f"{site} daytime clear (npts={pdf.shape[0]:,})")
+    filename = os.path.join("figures", f"LW_clr_{site}.png")
+    fig.savefig(filename, bbox_inches="tight", dpi=300)
+    plt.close()
+
+    print(pdf.shape[0])
+    rmse = np.sqrt(mean_squared_error(pdf.lw_s, pdf.lw_c))
+    print(rmse)
+    rmse = np.sqrt(mean_squared_error(pdf.lw_s, pdf.lw_c_22a))
+    print(rmse)
+    rmse = np.sqrt(mean_squared_error(pdf.dw_ir, pdf.lw_c_t))
+    print(rmse)
+    rmse = np.sqrt(mean_squared_error(pdf.lw_s, pdf.lw_c_t))
+    print(rmse)
+
+    # site = "BON"
+    # df = join_surfrad_asos(site)  # already downsampled data...
+    # df = df[["rh", "pressure", "t_a", "pw", "diffuse", "lw_s",
+    #          "esky_c", "lw_c", "zen"]]
+    # df = df.head(1000)
+    # df = df.loc[df.zen < 85]  # select daytime values
+    # df = df.tz_convert(None)  # make tz naive
+    # df = find_clearsky(df)
