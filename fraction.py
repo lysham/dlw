99cd74c04ebe558762395e5ce11a2adf15876a8b
Lysha Matsunobu <lmatsuno@eng.ucsd.edu>
2023-04-04 17:06:17 -0700

    fraction: add and test external and internal fraction functions

--- fraction.py
+++ fraction.py
@@ -32,17 +32,46 @@ def planck_lambda(l, t):
     return t1 / t2
 
 
-def de_wrt_dt(l, t):
-    t1 = np.exp(E_C2 / (l * t)) - 1
-    term1 = -5 * E_C1 / (np.power(l, 6) * t1)
-    t2 = np.exp(E_C2 / (l * t)) / (np.power(l, 7) * t * np.power(t1, 2))
-    term2 = E_C1 * E_C2 * t2
-    return term1 + term2
-
-
 def ext_fraction_lambda(l, t):
     const = 1 / (SIGMA * np.power(t, 4))
     out = integrate.quad(func=planck_lambda, a=0, b=l, args=(t,))[0]
+    return const * out
+
+
+def fe_lt(lt):
+    """External fractional function as a function of lambda * T (lt)
+
+    Parameters
+    ----------
+    lt : double
+        Lambda * T [micron * K]
+
+    Returns
+    -------
+    fe(lt)
+    """
+    const = E_C1 / (SIGMA * np.power(E_C2, 4))
+    y = lambda z: np.power(z, 3) / (np.exp(z) - 1)
+    out = integrate.quad(func=y, a=E_C2 / lt, b=256)[0]  # z to inf
+    return const * out
+
+
+def fi_lt(lt):
+    """Internal fractional function as a function of lambda * T (lt)
+    * some error at larger lt (e.g. order 10^-4 at lt of 10^5)
+
+    Parameters
+    ----------
+    lt : double
+        Lambda * T [micron * K]
+
+    Returns
+    -------
+    fi(lt)
+    """
+    const = E_C1 / (4 * SIGMA * np.power(E_C2, 4))
+    y = lambda z: (np.power(z, 4) * np.exp(z)) / np.power((np.exp(z) - 1), 2)
+    out = integrate.quad(func=y, a=E_C2 / lt, b=256)[0]  # z to inf
     return const * out
 
 
@@ -52,3 +81,6 @@ if __name__ == "__main__":
     t = 294.2
     out = integrate.quad(func=planck_lambda, a=3, b=50, args=(t,))[0]
     print(out)
+
+    # fraction function implementations imperfect (inf approximated as 256)
+    print(fi_lt(14300))  # 0.9875 Table 6.5
