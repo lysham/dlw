f4a643e9eaab2263753800a9f97420ccaae4c3c1
lysham <lmatsuno@eng.ucsd.edu>
2023-05-30 19:37:48 -0700

    fig3: look at first derivative of h2o and co2 correlations

--- fig3.py
+++ fig3.py
@@ -273,8 +273,8 @@ def plot_fig3_quantiles(site=None, yr=None, all_sites=False, tau=False,
 
     # Create dataset for training
     s = create_training_set(
-        year=yr, all_sites=all_sites, site=site, temperature=temperature,
-        cs_only=True, pct_clr_min=pct_clr_min
+        year=yr, temperature=temperature,
+        cs_only=True, filter_pct_clr=0.05, filter_npts_clr=0.2, drive="server4"
     )
     s = reduce_to_equal_pts_per_site(s)  # reduce to num pts in common
 
@@ -503,19 +503,74 @@ if __name__ == "__main__":
     #     yr=[2010, 2011, 2012, 2013], all_sites=True, tau=False,
     #     temperature=True, pct_clr_min=0.5, pressure_bins=5, violin=True
     # )
-    plot_fig3_quantiles(
-        yr=[2015], all_sites=True, tau=False,
-        temperature=True, pct_clr_min=0.3, pressure_bins=10, violin=True
-    )
-
-    # df = create_training_set(
-    #     year=[2012], all_sites=True, site=None, temperature=True,
-    #     cs_only=True, pct_clr_min=0.3
+    # plot_fig3_quantiles(
+    #     yr=[2015], tau=False,
+    #     temperature=False, pct_clr_min=0.05, pressure_bins=10, violin=True
     # )
-    # df = reduce_to_equal_pts_per_site(df)
-    #
-    # c1, c2, c3 = three_c_fit(df)
-    # print(c1, c2, c3)
-    #
-    # c1, c2, c3 = three_c_fit(df, c1=0.6376)
-    # print(c1, c2, c3)
+
+    pw = np.linspace(0.001, 2.5/100, 20)
+    b2_h2o = 0.1083 + 0.0748 * np.tanh(270.8944 * pw)
+    b3_h2o = -0.2308 + 0.6484 * np.power(pw, 0.1280)
+    b4_h2o = 0.0289 + 6.2436 * np.power(pw, 0.9010)
+    de_b2_h2o = 20.2629 * np.power(np.cosh(270.8944 * pw), -2)
+    de_b3_h2o = (0.6484 * 0.1280) * np.power(pw, 0.1280 - 1)
+    de_b4_h2o = (6.2436 * 0.9010) * np.power(pw, 0.9010 - 1)
+
+    b2_co2 = 0.0002
+    b3_co2 = 0.3038 - 0.5262 * np.power(pw, 0.1497)
+    b4_co2 = 0.0144 - 0.1740 * np.power(pw, 0.7268)
+    de_b2_co2 = np.zeros(len(pw))
+    de_b3_co2 = (-0.5262 * 0.1497) * np.power(pw, 0.1497 - 1)
+    de_b4_co2 = (-0.1740 * 0.7268) * np.power(pw, 0.7268 - 1)
+
+    # FOR PLOTTING
+    title = "B2"
+    h2o = b2_h2o
+    co2 = b2_co2
+    de_h2o = de_b2_h2o
+    de_co2 = de_b2_co2
+
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5), sharex=True)
+    ax = axes[0]
+    ax.grid(axis="y")
+    ax.fill_between(pw, 0, h2o, label="h2o", alpha=0.7)
+    ax.fill_between(pw, h2o, h2o + co2, label="co2", alpha=0.7)
+    ax.legend(ncol=2, bbox_to_anchor=(1.0, 1.01), loc="lower right")
+    ax.set_xlim(pw[0], pw[-1])
+    ax.set_ylim(0, 0.3)
+    ax.set_axisbelow(True)
+    ax.set_title(title, loc="left")
+    txt = r"$\varepsilon$"
+    ax.set_ylabel(txt)
+    ax.set_xlabel("dimensionless p$_w$")
+
+    ax = axes[1]
+    ax.grid(axis="y")
+    ax.plot(pw, de_h2o, label="h2o")
+    ax.plot(pw, de_co2, label="co2")
+    ax.plot(pw, de_h2o + de_co2, ls="--", c="0.5", label="sum")
+    ax.legend(ncol=3, bbox_to_anchor=(1.0, 1.01), loc="lower right")
+    txt = r"d$\varepsilon$ / dp$_w$"
+    ax.set_ylabel(txt)
+    ax.set_xlabel("dimensionless p$_w$")
+    plt.tight_layout()
+    plt.show()
+
+    fig, ax = plt.subplots(figsize=(5, 5))
+    ax.grid(axis="y")
+    ax.plot(pw, de_b2_h2o + de_b2_co2, label="B2 sum")
+    ax.plot(pw, de_b3_h2o + de_b3_co2, label="B3 sum")
+    ax.plot(pw, de_b4_h2o + de_b4_co2, label="B4 sum")
+    co2_total = de_b2_co2 + de_b3_co2 + de_b4_co2
+    h2o_total = de_b2_h2o + de_b3_h2o + de_b4_h2o
+    total = co2_total + h2o_total
+    ax.plot(pw, h2o_total, c="0.5", ls=":", label="co2 sum")
+    ax.plot(pw, co2_total, c="0.5", ls="--", label="h2o sum")
+    ax.plot(pw, total, c="0.1", ls="-", label="total")
+    ax.set_xlim(pw[0], pw[-1])
+    txt = r"d$\varepsilon$ / dp$_w$"
+    ax.set_ylabel(txt)
+    ax.set_xlabel("dimensionless p$_w$")
+    ax.legend(ncol=2, bbox_to_anchor=(0.5, 1.01), loc="lower center")
+    plt.tight_layout()
+    plt.show()
